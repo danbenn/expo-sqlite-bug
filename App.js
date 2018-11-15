34bcf0fbd67a95421bc6706004e7453cbd829f48
Daniel Bennett <danbenn@umich.edu>
2018-11-15 00:51:59 -0500

    added sample database and packager settings

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const sampleDatabase = require('./assets/sample.db');
+
 export default class App extends React.Component {
   render() {
     return (
